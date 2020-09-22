92d8ceb7718f0a8e28f1a64abc23982a709fa9ab
revsic <revsic99@gmail.com>
2020-09-23 02:30:59 +0900

    config: Reduce batch

--- dataset/config.py
+++ dataset/config.py
@@ -25,8 +25,8 @@ class Config:
         self.eps = 1e-5
 
         # sample size
-        self.frames = 16000
-        self.batch = 16
+        self.frames = 6400
+        self.batch = 8
 
     def window_fn(self):
         """Return window generator.
